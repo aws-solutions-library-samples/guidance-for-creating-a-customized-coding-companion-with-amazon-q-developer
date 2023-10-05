d9289e88e6fcee10834a0d9b5f7ea2a96373c234
jonslo <jonslo@amazon.com>
2023-10-05 15:02:44 -0500

    add comment for hard-coded region in environment

--- source/bin/custom-ai-code-generator.ts
+++ source/bin/custom-ai-code-generator.ts
@@ -5,6 +5,7 @@ import { CustomAiCodeGeneratorStack } from '../lib/custom-ai-code-generator-stac
 
 const app = new cdk.App();
 new CustomAiCodeGeneratorStack(app, 'CustomAiCodeGeneratorStack', {
+  // Amazon CodeWhisperer is hosted in us-east-1, hence the hard-coded region for this AWS environment.
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
